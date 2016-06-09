bd79877e706ebfa59175dfa581ae963021c638dc
Connor Lee <connor.lee@contrastsecurity.com>
2016-06-09 14:21:28 -0400

    Added texbox and basic POST HTTP support

--- src/main/java/com/contrastsecurity/ConnectionServlet.java
+++ src/main/java/com/contrastsecurity/ConnectionServlet.java
@@ -1,5 +1,4 @@
 package com.contrastsecurity;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.servlet.*;
@@ -7,19 +6,56 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.user.UserManager;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+
+@Named("Teamserver Connect")
 public class ConnectionServlet extends HttpServlet
 {
-
-    private static final Logger log = LoggerFactory.getLogger(ConnectionServlet.class);
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-
-    {
-		resp.setContentType("text/html");
-		resp.getWriter().write("<html><body>Hello! You did it.</body></html>");
-
-    }
-
+	private static final Logger log = LoggerFactory.getLogger(ConnectionServlet.class);
+	
+	@ComponentImport
+	private final UserManager userManager;
+	@ComponentImport
+	private final LoginUriProvider loginUriProvider;
+	
+	@Inject
+	public ConnectionServlet(UserManager userManager,
+			LoginUriProvider loginUriProvider) {
+		this.userManager = userManager;
+		this.loginUriProvider = loginUriProvider;
+	}
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+	{
+		String username = userManager.getRemoteUsername(request);
+		if (username == null || !userManager.isSystemAdmin(username))
+		{
+			redirectToLogin(request, response);
+			return;
+		}
+		response.setContentType("text/html");
+		response.getWriter().write("<html><body>Hi again! Looking good.</body></html>");
+	}
+	
+	private void redirectToLogin(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		response.sendRedirect(loginUriProvider.getLoginUri(getUri(request)).toASCIIString());
+	}
+	private URI getUri(HttpServletRequest request)
+	{
+		StringBuffer builder = request.getRequestURL();
+		if (request.getQueryString() != null)
+		{
+			builder.append("?");
+			builder.append(request.getQueryString());
+		}
+		return URI.create(builder.toString());
+	} 
 }
