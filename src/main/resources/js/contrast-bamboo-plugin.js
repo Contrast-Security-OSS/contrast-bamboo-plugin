94c7c9e610071a6548b87c5fa582afcfa185c0a9
Connor Lee <connor.lee@contrastsecurity.com>
2016-06-16 11:56:39 -0400

    Will now populate from the backend, but currently doesn't PUT. Working on that now.

--- src/main/resources/js/contrast-bamboo-plugin.js
+++ src/main/resources/js/contrast-bamboo-plugin.js
@@ -1,33 +1,31 @@
 window.onload  = function() {
 	var baseUrl = "/bamboo";
 	function populateForm() {
-		console.log("sending ajax call");
 		AJS.$.ajax({
 			url: baseUrl + "/rest/teamserver-admin/1.0/",
 			dataType: "json",
 			success: function(config) {
-				console.log("ajax success");
 				AJS.$("#username").val(config.username);
-				AJS.$("#apikey").val(config.apikey);
-				AJS.$("#servicekey").val(config.servicekey);
-				AJS.$("#url").val(config.url);    
+				AJS.$("#apiKey").val(config.apikey);
+				AJS.$("#serviceKey").val(config.servicekey);
+				AJS.$("#url").val(config.url);
 			},
 			error: function(jqXHR, textStatus, errorThrown) {
-		        console.log(jqXHR.responseText);
-		        console.log(textStatus);
-		        console.log(errorThrown);
+				console.log(jqXHR.responseText);
+				console.log(textStatus);
+				console.log(errorThrown);
 			}
 		});
 	}
 	function updateConfig() {
 		var user = AJS.$("#username").attr("value");
-		var api = AJS.$("#apikey").attr("value");
-		var service = AJS.$("#servicekey").attr("value");
+		var api = AJS.$("#apiKey").attr("value");
+		var service = AJS.$("#serviceKey").attr("value");
 		var url = AJS.$("#url").attr("value");
 		AJS.$.ajax({
 			url: baseUrl + "/rest/teamserver-config/1.0/",
 			type: "PUT",
-			contentType: "application/json",
+			contentType: "json",
 			data:{ 
 			"username": user,
 			"apikey": api,
@@ -36,12 +34,14 @@ window.onload  = function() {
 		},
 		processData: false
 		});
-		alert(0);
 	}  
+	AJS.$("#admin-submit").removeAttr('onsubmit').submit(function(event){
+        event.preventDefault();
+    });
 	populateForm();
-
-	AJS.$("#admin").submit(function(e) {
-		e.preventDefault();
+    AJS.$('#admin-submit').click(function(){
+		alert(0);
 		updateConfig();
+		return false;
 	});
 };
