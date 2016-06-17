a3b559dd9afd6efd6de5cbfb4884c6a03bd67c3f
Connor Lee <connor.lee@contrastsecurity.com>
2016-06-17 14:06:23 -0400

    Storage and retieval of teamserver configuration now works

--- src/main/java/com/contrastsecurity/ConfigResource.java
+++ src/main/java/com/contrastsecurity/ConfigResource.java
@@ -18,9 +18,6 @@ import com.atlassian.sal.api.transaction.TransactionCallback;
 import com.atlassian.sal.api.transaction.TransactionTemplate;
 import com.atlassian.sal.api.user.UserManager;
 
-import java.util.Enumeration;
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -65,17 +62,12 @@ public class ConfigResource
 		{
 			public Object doInTransaction()
 			{
-				//PluginSettings settings = pluginSettingsFactory.createGlobalSettings();
+				PluginSettings settings = pluginSettingsFactory.createGlobalSettings();
 				TeamserverProfile config = new TeamserverProfile();
-				/*config.setUsername((String) settings.get(PLUGIN_STORAGE_KEY + ".username"));				
+				config.setUsername((String) settings.get(PLUGIN_STORAGE_KEY + ".username"));				
 				config.setApikey((String) settings.get(PLUGIN_STORAGE_KEY + ".apikey"));
 				config.setServicekey((String) settings.get(PLUGIN_STORAGE_KEY + ".servicekey"));
 				config.setUrl((String) settings.get(PLUGIN_STORAGE_KEY + ".url"));
-				 */
-				config.setUsername("configUsername");	
-				config.setApikey("configApi");
-				config.setServicekey("configServ");
-				config.setUrl("configURL");
 				return config;
 			}
 		})).build();
@@ -83,24 +75,9 @@ public class ConfigResource
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response post(final String config, @Context HttpServletRequest request)
+	public Response post(final TeamserverProfile config, @Context HttpServletRequest request)
 	{
 		System.out.println(config);
-		System.out.println(request);
-
-		Enumeration<String> parameterNames = request.getParameterNames();
-		while (parameterNames.hasMoreElements()) {
-			String paramName = parameterNames.nextElement();
-			System.out.println(paramName);
-
-			String[] paramValues = request.getParameterValues(paramName);
-
-			for (int i = 0; i < paramValues.length; i++) {
-				String paramValue = paramValues[i];
-
-				System.out.println("\t" + paramValue);
-			}
-		}
 
 		String username = userManager.getRemoteUsername(request);
 		if (username == null || !userManager.isSystemAdmin(username))
@@ -108,7 +85,7 @@ public class ConfigResource
 			return Response.status(Status.UNAUTHORIZED).build();
 		}
 
-		/*transactionTemplate.execute(new TransactionCallback<Object>()
+		transactionTemplate.execute(new TransactionCallback<Object>()
 		{
 			public Object doInTransaction()
 			{
@@ -120,7 +97,7 @@ public class ConfigResource
 				pluginSettings.put(PLUGIN_STORAGE_KEY  +".url", config.getUrl());
 				return null;
 			}
-		});*/
+		});
 		return Response.noContent().build();
 	}
 
