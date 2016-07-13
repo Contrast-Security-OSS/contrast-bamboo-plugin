0b26c216b05b79e946923b6dd68286aff9500b52
Connor Lee <connor.lee@contrastsecurity.com>
2016-07-13 16:12:08 -0400

    created hello world execute method for base task

---
+++ src/main/java/com/contrastsecurity/VerifyThresholdsTask.java
@@ -0,0 +1,20 @@
+package com.contrastsecurity;
+import com.atlassian.bamboo.build.logger.BuildLogger;
+import com.atlassian.bamboo.task.TaskContext;
+import com.atlassian.bamboo.task.TaskException;
+import com.atlassian.bamboo.task.TaskResult;
+import com.atlassian.bamboo.task.TaskResultBuilder;
+import com.atlassian.bamboo.task.TaskType;
+import org.jetbrains.annotations.NotNull;
+
+public class VerifyThresholdsTask implements TaskType{
+
+    @NotNull
+    public TaskResult execute(@NotNull TaskContext taskContext) throws TaskException {
+        final BuildLogger buildLogger = taskContext.getBuildLogger();
+
+        buildLogger.addBuildLogEntry("Hello, World!");
+
+        return TaskResultBuilder.newBuilder(taskContext).success().build();
+    }
+}
